5aa503e9c4b3f9c86aa2d1d945d48194fa636b0d
AdityaDew26 <songoku739529@gmail.com>
2025-01-28 12:04:10 +0530

    file updated

--- Client/src/components/Landing/home/Home.jsx
+++ Client/src/components/Landing/home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import Navbar from '../Navbar/Nabvar'
 import Hero from '../hero/Hero'
 import About from '../About/About'
@@ -8,6 +8,23 @@ import Contact from '../contact/Contact'
 
 
 const Home = () => {
+
+   useEffect(() => {
+    const handleScroll = () => {
+      const elements = document.querySelectorAll('.slide-up');
+      elements.forEach((el) => {
+        const rect = el.getBoundingClientRect();
+        if (rect.top < window.innerHeight - 100) {
+          el.classList.add('active');
+        }
+      });
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // Cleanup event listener on unmount
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
   return (
     <div>
       <Navbar/>
